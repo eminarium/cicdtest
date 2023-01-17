3e7289cb110223131f87c50707a6dd93f4697e04
Merdan D <merdan.durdiyev@gmail.com>
2023-01-17 15:23:53 +0500

    Adds post model unit tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,22 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not be valid without a title" do
+    post = Post.new(content: "Just a test content")
+
+    assert_not post.valid?, "Post is valid without a title..."
+  end
+
+  test "should not be valid without a conten" do
+    post = Post.new(title: "Just a test title")
+
+    assert_not post.valid?, "Post is valid without a content..."
+  end
+
+  test "should be valid with valid details" do
+    post = Post.new(title: "A valid title", content: "A valid post content")
+
+    assert post.valid?, "Post is not valid with valid details..."
+  end
 end
